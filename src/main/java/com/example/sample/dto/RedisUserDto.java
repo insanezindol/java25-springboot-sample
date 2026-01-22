d00e8eee61360057c7003c2a54125dc298479212
dean <leejinhyung90@gmail.com>
2026-01-22 22:33:23 +0900

    fix : record 변경

--- src/main/java/com/example/sample/dto/RedisUserDto.java
+++ src/main/java/com/example/sample/dto/RedisUserDto.java
@@ -1,26 +1,20 @@
 package com.example.sample.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class RedisUserDto {
-    private String id;
-    private String username;
-    private String email;
-    private int age;
+public record RedisUserDto(
+        String id,
+        String username,
+        String email,
+        int age,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        LocalDateTime createdAt,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        LocalDateTime updatedAt
+) {
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime createdAt;
-
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime updatedAt;
 }
